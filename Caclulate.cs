a123a7ee4d0b410430087e241d8e5bb49b64fb54
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2025-02-24 16:09:11 +0200

    Add files via upload

---
+++ Caclulate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Класс описывающий вычисления
+    /// </summary>
+    class Caclulate
+    {
+        /// <summary>
+        /// Поле первого числа
+        /// </summary>
+        private double field1;
+        /// <summary>
+        /// Поле второго числа
+        /// </summary>
+        private double field2;
+
+        public double Field1 { get => field1; set { if (value < 0) throw new ArgumentException("Error"); field1 = value; } }
+        public double Field2 { get => field2; set { if (value < 0) throw new ArgumentException("Error"); field2 = value; } } 
+
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Caclulate()
+        { }
+        /// <summary>
+        /// Конструктор с одним параметром
+        /// </summary>
+        /// <param name="field1">поле1</param>
+        public Caclulate(double field1)
+        {
+            this.Field1 = field1;
+        }
+        /// <summary>
+        /// Конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="field1">поле1</param>
+        /// <param name="field2">поле2</param>
+        public Caclulate(double field1, double field2) :this(field1)
+        {
+            this.Field2 = field2;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"field1={Field1}, field2={Field2}");
+        }
+        public double ToString()
+        {
+            return Math.Sqrt(Field1 * Field2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Caclulate calc1 = new Caclulate();
+            calc1.Info();
+            Console.WriteLine($"{calc1.ToString()}");
+            calc1.Field1 = 34;
+            calc1.Field2 = 14;
+            calc1.Info();
+            Console.WriteLine($"{calc1.ToString()}");
+            Caclulate calc2 = new Caclulate(9);
+            calc2.Info();
+            Caclulate calc3 = new Caclulate(9, 12);
+            calc3.Info();
+            Console.WriteLine($"{calc3.ToString()}");
+            Console.Write($"Введите первое число: ");
+            double field1 = double.Parse(Console.ReadLine());
+            Console.Write($"Введите второе число: ");
+            double field2 = double.Parse(Console.ReadLine());
+            Caclulate calc4 = new Caclulate(field1, field2);
+            Console.WriteLine($"{calc4.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
