b03e16792d569e4889b3bb5ecc9f5f37e55d65e2
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2025-02-24 16:09:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            rect1.Length=23;
+            rect1.Width=13;
+            rect1.Info();
+            Console.WriteLine($"{rect1.ToString()}");
+            Console.WriteLine();
+            Rectangle rect2 = new Rectangle(4,2);
+            rect2.Info();
+            Console.WriteLine($"{rect2.ToString()}");
+            Console.WriteLine();
+            Console.Write("Длина: ");
+            int Length = int.Parse(Console.ReadLine());
+            Console.Write("Ширина: ");
+            int Width = int.Parse(Console.ReadLine());
+            Rectangle rect3 = new Rectangle(Length, Width);
+            Console.WriteLine($"{rect3.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Rectangle
+    {
+        private int length;
+        private int width;
+        public Rectangle() { }
+        public Rectangle(int length)
+        { this.Length = length; }
+        public Rectangle(int length, int width) : this(length)
+        {this.Width=width;}
+
+        public int Length { get => length; set { 
+                if (value < 0)
+                    throw new ArgumentException("Error");
+                length = value; }
+            }
+        public int Width { get => width; set { 
+                if (value < 0)
+                    throw new ArgumentException("Error");
+                width = value;}
+          }
+        public void Info()
+        {
+            Console.WriteLine($"Длина:{Length} Длина:{Width}");
+        }
+        public override string ToString()
+        {
+            return $"Площадь{Length * Width:f1}\nПериметр:{2 * (Length * Width):f1}";
+        }
+    }
+}
